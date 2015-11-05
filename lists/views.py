ef0b265bf131cdf44b48ce5a46a6f1085a729b09
Raphael Eicher <raphael.eicher@gmail.com>
2015-11-05 20:02:38 +0100

    page 69

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from lists.models import Item
+
+# ToDo: Support more than one list!
 
 
 def home_page(request):
-    return render(request, 'lists/home.html', {
-        'new_item_text': request.POST.get('item_text', ''),
-    })
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect('/')
+    else:
+        return render(request, 'lists/home.html')

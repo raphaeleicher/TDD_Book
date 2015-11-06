e851d84e20a3b7252e440f8d65107a7b9b21dcdc
Raphael Eicher <raphael.eicher@gmail.com>
2015-11-06 23:18:59 +0100

    page 90

--- lists/views.py
+++ lists/views.py
@@ -6,7 +6,12 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST['item_text']
         Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
     else:
         items = Item.objects.all()
         return render(request, 'lists/home.html', {'items': items})
+
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'lists/home.html', {'items': items})
